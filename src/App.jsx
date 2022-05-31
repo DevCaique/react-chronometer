34ea9e031729dfd4dadac12e10fd017e9c67781a
Caique <aecaique@gmail.com>
2022-05-30 22:28:32 -0300

    functionalities made

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+// Dependecies
+import { useState } from "react";
+
+// Style
+import "./App.css";
+
+export function App() {
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [intervalId, setIntervalId] = useState(0);
+
+  const startStop = () => {
+    if (intervalId) {
+      clearInterval(intervalId);
+      setIntervalId(0);
+      return;
+    }
+
+    const newIntervalId = setInterval(() => {
+      setSeconds((state) => state + 1);
+    }, 1000);
+    setIntervalId(newIntervalId);
+  };
+
+  const reset = () => {
+    clearInterval(intervalId);
+    setIntervalId(0);
+    setSeconds(0);
+    setMinutes(0);
+    return;
+  };
+
+  if (seconds > 60) {
+    setSeconds(0);
+    setMinutes((state) => state + 1);
+  }
+
+  return (
+    <div className="application">
+      <div className="timer__area">
+        <div className="output">
+          <span>{String(minutes).padStart(2, "0")}</span>:
+          <span>{String(seconds).padStart(2, "0")}</span>
+        </div>
+        <div className="btn__area">
+          <button onClick={startStop}>
+            {intervalId ? "parar" : "iniciar"}
+          </button>
+          <button onClick={reset}>resetar</button>
+        </div>
+      </div>
+    </div>
+  );
+}
